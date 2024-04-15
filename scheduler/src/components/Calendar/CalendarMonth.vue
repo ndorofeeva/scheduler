53edfbd937c3378b00b1c6f8ee27e0ff5deab128
ndorofeeva <nat.muovi@gmail.com>
2024-04-15 15:09:29 +0200

    Cleanup CalendarMonth
    Make logic more readable and replace css styles with TailwindCss classes.

--- scheduler/src/components/Calendar/CalendarMonth.vue
+++ scheduler/src/components/Calendar/CalendarMonth.vue
@@ -6,37 +6,46 @@ const props = defineProps<{
   month: number;
 }>()
 
-const columnNumber = 7;
+const SUNDAY_INDEX = 0;
+const SATURDAY_INDEX = 6;
+const ROWS_PER_MONTH = 5;
+const DAYS_IN_WEEK = 7;
 
-const firstDayOfMonth = computed(() => new Date(props.year, props.month, 1).getDay());
-const lastDayOfMonth = computed(() => new Date(props.year, props.month + 1, 0).getDate());
-const lastDayOfPrevMonth = computed(() => new Date(props.year, props.month, 0).getDate());
+const columnNumber = DAYS_IN_WEEK;
+const firstDayOfMonthIndex = computed(() => new Date(props.year, props.month, 1).getDay());
+const currentMonthDaysCount = computed(() => new Date(props.year, props.month + 1, 0).getDate());
 
-const rowNumber = computed(() =>
-  ((firstDayOfMonth.value === 0 && (lastDayOfMonth.value === 31 || lastDayOfMonth.value === 30)) ||
-    (firstDayOfMonth.value === 6 && lastDayOfMonth.value === 31)) ? 6 : 5);
+const lastMonthDaysCount = computed(() =>
+  firstDayOfMonthIndex.value === SUNDAY_INDEX ? DAYS_IN_WEEK - 1 : firstDayOfMonthIndex.value - 1);
 
-const allDaysCount = computed(() => rowNumber.value * columnNumber);
-
-let lastMonthDaysCount = computed(() => firstDayOfMonth.value === 0 ? 6 : firstDayOfMonth.value - 1);
-const daysLastMonth = computed(() => {
-  let days = [];
+function* genLastMonthDays(lastDayOfPrevMonth: number) {
   for (var i = lastMonthDaysCount.value - 1; i >= 0; i--) {
-    days.push(lastDayOfPrevMonth.value - i);
+    yield lastDayOfPrevMonth - i;
   }
-  return days;
+}
+const lastMonthDaysArray = computed(() => {
+  const lastDayOfPrevMonth = new Date(props.year, props.month, 0).getDate();
+  return genLastMonthDays(lastDayOfPrevMonth);
 });
 
+const nextMonthDaysCount = computed(() => {
+  const isExtraWeekNeeded =
+    (firstDayOfMonthIndex.value === SUNDAY_INDEX &&
+      (currentMonthDaysCount.value === 31 || currentMonthDaysCount.value === 30)
+    ) ||
+    (firstDayOfMonthIndex.value === SATURDAY_INDEX && currentMonthDaysCount.value === 31);
+  const rowNumber = isExtraWeekNeeded ? ROWS_PER_MONTH + 1 : ROWS_PER_MONTH;
+  return rowNumber * columnNumber - currentMonthDaysCount.value - lastMonthDaysCount.value
+});
 
-const nextMonthDaysCount = computed(() => allDaysCount.value - lastDayOfMonth.value - lastMonthDaysCount.value);
 </script>
 
 <template>
-  <div class="calendar">
-    <div v-for="day in daysLastMonth" :key="day" class="text-gray-300 text-center">
+  <div class="grid grid-cols-7 gap-4">
+    <div v-for="day in lastMonthDaysArray" :key="day" class="text-gray-300 text-center">
       {{ day }}
     </div>
-    <div v-for="day in lastDayOfMonth" :key="day" class="text-center">
+    <div v-for="day in currentMonthDaysCount" :key="day" class="text-center">
       {{ day }}
     </div>
     <div v-for="day in nextMonthDaysCount" :key="day" class="text-gray-300 text-center">
@@ -44,11 +53,3 @@ const nextMonthDaysCount = computed(() => allDaysCount.value - lastDayOfMonth.va
     </div>
   </div>
 </template>
-
-<style scoped>
-.calendar {
-  display: grid;
-  grid-gap: 10px;
-  grid-template-columns: repeat(7, 1fr);
-}
-</style>
