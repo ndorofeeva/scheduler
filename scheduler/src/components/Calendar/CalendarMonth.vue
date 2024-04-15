81e6ecd716da1adbc131eb44751c6559183a602a
ndorofeeva <nat.muovi@gmail.com>
2024-04-15 10:03:40 +0200

    Create Calendar view draft

--- scheduler/src/components/Calendar/CalendarMonth.vue
+++ scheduler/src/components/Calendar/CalendarMonth.vue
@@ -1,12 +1,54 @@
 <script setup lang="ts">
-defineProps<{
+import { computed } from 'vue';
+
+const props = defineProps<{
+  year: number;
+  month: number;
 }>()
+
+const columnNumber = 7;
+
+const firstDayOfMonth = computed(() => new Date(props.year, props.month, 1).getDay());
+const lastDayOfMonth = computed(() => new Date(props.year, props.month + 1, 0).getDate());
+const lastDayOfPrevMonth = computed(() => new Date(props.year, props.month, 0).getDate());
+
+const rowNumber = computed(() =>
+  ((firstDayOfMonth.value === 0 && (lastDayOfMonth.value === 31 || lastDayOfMonth.value === 30)) ||
+    (firstDayOfMonth.value === 6 && lastDayOfMonth.value === 31)) ? 6 : 5);
+
+const allDaysCount = computed(() => rowNumber.value * columnNumber);
+
+let lastMonthDaysCount = computed(() => firstDayOfMonth.value === 0 ? 6 : firstDayOfMonth.value - 1);
+const daysLastMonth = computed(() => {
+  let days = [];
+  for (var i = lastMonthDaysCount.value - 1; i >= 0; i--) {
+    days.push(lastDayOfPrevMonth.value - i);
+  }
+  return days;
+});
+
+
+const nextMonthDaysCount = computed(() => allDaysCount.value - lastDayOfMonth.value - lastMonthDaysCount.value);
 </script>
 
 <template>
   <div class="calendar">
-
+    <div v-for="day in daysLastMonth" :key="day" class="text-gray-300 text-center">
+      {{ day }}
+    </div>
+    <div v-for="day in lastDayOfMonth" :key="day" class="text-center">
+      {{ day }}
+    </div>
+    <div v-for="day in nextMonthDaysCount" :key="day" class="text-gray-300 text-center">
+      {{ day }}
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.calendar {
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: repeat(7, 1fr);
+}
+</style>
--- scheduler/src/views/HomeView.vue
+++ scheduler/src/views/HomeView.vue
@@ -1,16 +1,48 @@
 <script setup lang="ts">
+import { reactive } from 'vue';
+import CalendarMonth from '../components/Calendar/CalendarMonth.vue'
+const date = new Date();
+const currentMonth = date.getMonth();
+const react = reactive({
+  month: currentMonth
+})
+const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
 </script>
 
 <template>
   <main>
     <div class="grid dark">
-      <div class="left-sidebar dark:bg-black">
+      <div class="left-sidebar border border-indigo-600 p-5">
         Left
       </div>
-      <div class="content">
-        Center
+      <div class="content border border-indigo-600 p-5">
+        <div class="flex justify-between">
+          <div @click="react.month = react.month - 1" class="text-pink-300 cursor-pointer"> Prev </div>
+          <div class="text-pink-600">{{ months[react.month] }}</div>
+          <div @click="react.month = react.month + 1" class="text-pink-300 cursor-pointer"> Next </div>
+        </div>
+        <div class="week-days">
+          <div v-for="(day, index) in weekDays" :key="index" class="text-green-300 text-center">
+            {{ day }}
+          </div>
+        </div>
+        <CalendarMonth :year="date.getFullYear()" :month="react.month" />
       </div>
-      <div class="right-sidebar">
+      <div class="right-sidebar border border-indigo-600 p-5">
         Right
       </div>
     </div>
@@ -26,16 +58,16 @@
 
   .left-sidebar {
     grid-area: left-sidebar;
-    background-color: blueviolet;
   }
 
   .right-sidebar {
     grid-area: right-sidebar;
-    background-color: aquamarine;
   }
+}
 
-  .content {
-    background-color: aqua;
-  }
+.week-days {
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: repeat(7, 1fr);
 }
 </style>
